c2064e5daab7043c3ddf722064655ef75aa2e87c
omerdombaycioglu <110188931+omerdombaycioglu@users.noreply.github.com>
2025-05-11 01:23:36 +0300

    Update index.php
    Signed-off-by: omerdombaycioglu <110188931+omerdombaycioglu@users.noreply.github.com>

--- web/index.php
+++ web/index.php
@@ -1,39 +1,44 @@
 <?php
-
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Log\LoggerInterface;
-use DI\Container;
-use DI\Bridge\Slim\Bridge;
-use Monolog\Handler\StreamHandler;
-use Monolog\Level;
-use Monolog\Logger;
-use Slim\Factory\AppFactory;
-use Slim\Views\Twig;
 
-require(__DIR__.'/../vendor/autoload.php');
+// Yol tanımlarını düzeltin
+require __DIR__ . '/vendor/autoload.php';
 
-// Create DI container
-$container = new Container();
-// Add Twig to Container
+// Container oluşturma
+$container = new DI\Container();
+
+// Twig ayarları (Railway'e uygun)
 $container->set(Twig::class, function() {
-  return Twig::create(__DIR__.'/../views');
-});
-// Add Monolog to Container
-$container->set(LoggerInterface::class, function () {
-  $logger = new Logger('default');
-  $logger->pushHandler(new StreamHandler('php://stderr'), Level::Debug);
-  return $logger;
+    $twig = Twig::create(__DIR__ . '/views', [
+        'cache' => false // Railway'de cache sorunları olabilir
+    ]);
+    return $twig;
 });
 
-// Create main Slim app
-$app = Bridge::create($container);
-$app->addErrorMiddleware(true, false, false);
-
-// Our web handlers
-$app->get('/', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
-  $logger->debug('logging output.');
-  return $twig->render($response, 'index.twig');
+// Logger ayarları
+$container->set(Psr\Log\LoggerInterface::class, function () {
+    $logger = new Monolog\Logger('app');
+    $logger->pushHandler(new Monolog\Handler\StreamHandler('php://stderr', Monolog\Logger::DEBUG));
+    return $logger;
 });
+
+// Uygulamayı oluştur
+$app = DI\Bridge\Slim\Bridge::create($container);
+
+// Error middleware
+$app->addErrorMiddleware(
+    $_ENV['APP_ENV'] === 'development',
+    true,
+    true
+);
+
+// Ana route
+$app->get('/', function (Request $request, Response $response) use ($container) {
+    return $container->get(Twig::class)->render($response, 'index.twig');
+});
+
+// Çalıştır
+$app->run();
 
 $app->run();
